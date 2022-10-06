30c8b7eaf329a8df55f341c006ebbfea5857f214
francis-xavier-inyangat <xavierfranzings@gmail.com>
2022-10-06 18:00:04 +0300

    toggle nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,21 @@ import Logo from '../images/logo.png'
 import './navbar.css'
 import {links} from '../data'
 import {GoThreeBars} from 'react-icons/go'
+import {MdOutlineClose} from 'react-icons/md'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+    const [isNavShowing, setisNavShowing] = useState(false);
+   
+
   return (
     <nav>
         <div className="container nav__container">
             <Link to='/' className='logo'>
                 <img src={Logo} alt="Nav Logo" />
             </Link>
-            <ul className='nav__links'>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
                 {
                     links.map(({name, path}, index)=>{
                         return (
@@ -25,8 +30,10 @@ const Navbar = () => {
                 }
             </ul>
               
-            <button className="nav__toggle-btn">
-                <GoThreeBars/>
+            <button className="nav__toggle-btn" onClick={()=>setisNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/>: <GoThreeBars/>
+                }
             </button>
         </div>
     </nav>
